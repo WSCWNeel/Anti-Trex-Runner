df8cf25e3ac219c1f93fe5c11b1937a7faff832d
Rishi10898 <sriramr@wsc.school.nz>
2024-08-19 11:20:54 +1200

    lol

---
+++ Scene/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 550.0
+const JUMP_VELOCITY = -350.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite = $AnimatedSprite2D 
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left","move_right")
+	
+	if direction > 0: 
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = false
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
