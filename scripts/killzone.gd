cf36f49cf0c4e920e6ee913f6a91bbe57eae01a9
Rishi10898 <sriramr@wsc.school.nz>
2024-08-23 17:13:31 +1200

    hi

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,17 @@ extends Area2D
 @onready var timer = $Timer
 func _on_body_entered(body):
 	print("You died")
+<<<<<<< Updated upstream
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+=======
+>>>>>>> Stashed changes
 	timer.start()
 
 
 func _on_timer_timeout():
+<<<<<<< Updated upstream
 	Engine.time_scale = 1.0
+=======
+>>>>>>> Stashed changes
 	get_tree().reload_current_scene()	
