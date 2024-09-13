9fc03a038439d6c6e93223b6a0039eadffb1acdb
Rishi10898 <sriramr@wsc.school.nz>
2024-09-13 18:36:19 +1200

    hi

---
+++ Scene/game_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+var score = 0
+func add_point():
+	socre += 1 
+	print(score)
+	
---
+++ Scene/main_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_quit_game_pressed():
+	get_tree().quit()
+
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://Scene/game.tscn")
