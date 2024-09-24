afe9425fcb431373f467dabf888059db423bbac3
Rishi10898 <sriramr@wsc.school.nz>
2024-09-25 11:56:09 +1200

    hi

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -2,9 +2,18 @@ extends Node2D
 
 
 
-func _on_quit_game_pressed():
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Scene/game.tscn")
+
+func _on_QuitButton_pressed():
 	get_tree().quit()
 
+func _on_PauseButton_pressed():
+	get_tree().paused = true
+	$PauseButton.hide()
+	$ResumeButton.show()
 
-func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://Scene/game.tscn")
+func _on_ResumeButton_pressed():
+	get_tree().paused = false
+	$ResumeButton.hide()
+	$PauseButton.show()
