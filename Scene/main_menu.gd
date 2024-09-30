5e145e48bb637be3740d7012c050e5668be26617
Rishi10898 <sriramr@wsc.school.nz>
2024-09-30 20:26:37 +1300

    hi

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -1,10 +1,8 @@
 extends Control
 
-func _process(delta):
-	pass
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scene/game.tscn")
+	get_tree().change_scene("res://Scene/game.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
